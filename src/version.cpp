c941b3bdc591398bb10e8c6d3ab3c2227d93330d
Ben Baker <bakerb845@gmail.com>
2021-04-05 09:28:38 -0600

    adding version

---
+++ src/version.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include "eikonalxx/version.hpp"
+
+using namespace MFLib;
+
+int Version::getMajor() noexcept
+{
+    return EIKONALXX_MAJOR;
+}
+
+int Version::getMinor() noexcept
+{
+    return EIKONALXX_MINOR;
+}
+
+int Version::getPatch() noexcept
+{
+    return EIKONALXX_PATCH;
+}
+
+bool Version::isAtLeast(const int major, const int minor,
+                        const int patch) noexcept
+{
+    if (EIKONALXX_MAJOR < major){return false;}
+    if (EIKONALXX_MAJOR > major){return true;}
+    if (EIKONALXX_MINOR < minor){return false;}
+    if (EIKONALXX_MINOR > minor){return true;}
+    if (EIKONALXX_PATCH < patch){return false;}
+    return true;
+}
+
+std::string Version::getVersion() noexcept
+{
+    std::string version(std::to_string(getMajor()) + "."
+                      + std::to_string(getMinor()) + "."
+                      + std::to_string(getPatch()));
+    return version;
+}
