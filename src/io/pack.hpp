08e45937aa00ad90a4d6e904d4fa3e7ef8fc0e6f
Ben Baker <bakerb845@gmail.com>
2023-10-13 13:25:32 -0600

    removing unnecessary header

--- src/io/pack.hpp
+++ src/io/pack.hpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-#include <bit>
 
 namespace
 {
@@ -15,6 +14,7 @@ std::string fillBlanksWithUnderscores(const std::string &s)
     return result;
 }
 
+[[maybe_unused]]
 void pack(const float f4, char c4[4])
 {
     auto cTemp = reinterpret_cast<const char *> (&f4);
@@ -31,11 +31,13 @@ void pack(const float f4, char c4[4])
     }   
 }
 
+[[maybe_unused]]
 void pack(const double f8, char c4[4])
 {
     return ::pack(static_cast<float> (f8), c4);
 }
 
+[[maybe_unused]]
 void pack(const size_t n, const float *__restrict__ x,
           char *__restrict__ c4) 
 {
@@ -45,6 +47,7 @@ void pack(const size_t n, const float *__restrict__ x,
     }   
 }
 
+[[maybe_unused]]
 void pack(const size_t n, const double *__restrict__ x,
           char *__restrict__ c4) 
 {
@@ -54,6 +57,7 @@ void pack(const size_t n, const double *__restrict__ x,
     }
 }
 
+[[maybe_unused]]
 std::vector<char> pack(const std::vector<float> &x) 
 {
     std::vector<char> cx(x.size()*4);
@@ -61,14 +65,14 @@ std::vector<char> pack(const std::vector<float> &x)
     return cx; 
 }
 
-/*
+[[maybe_unused]]
 std::vector<char> pack(const std::vector<double> &x) 
 {
-    std::vector<char> cx(x.size()*4);
+    std::vector<char> cx(x.size()*8);
     ::pack(x.size(), x.data(), cx.data());
     return cx; 
 }
-*/
+
 
 }
 #endif
